0824f06a79df1ff1fa743209b0c256476a2b43a5
Alison Oliveira <alison.oliveira89@gmail.com>
2025-05-11 21:58:39 -0300

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import './globals.css'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { Toaster } from 'react-hot-toast'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Seu Projeto',
+  description: 'Descrição do projeto',
+  icons: {
+    icon: '/x-logo.ico',
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
